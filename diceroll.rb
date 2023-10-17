a651a38bce035f1f1216e33fcb798facb2f60746
bellamahoney <146144909+bellamahoney@users.noreply.github.com>
2023-10-17 16:34:03 +0000

    dice rolls more

--- diceroll.rb
+++ diceroll.rb
@@ -40,7 +40,20 @@ get "/dice/1/20" do
   
   outcome = "You rolled a #{first_die} for a total of #{sum}"
   
-  "<h1>2d10</h1>
+  "<h1>1d20</h1>
   <p1>#{outcome}</p1>"
   end 
-  
+
+  get "/dice/5/4" do
+    first_die = rand(1..4)
+    second_die = rand(1..4)
+    third_die = rand(1..4)
+    fourth_die = rand(1..4)
+    fifth_die = rand(1..4)
+    sum = first_die + second_die +third_die + fourth_die + fifth_die
+    
+    outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}"
+    
+    "<h1>5d4</h1>
+    <p1>#{outcome}</p1>"
+    end 
